a3540083af50220376aa7768c59e03a26d2c688f
augerm <michaelauger23@gmail.com>
2021-05-09 19:31:32 -0700

    Fix bugs and clean up regex logic

--- src/index.ts
+++ src/index.ts
@@ -41,9 +41,20 @@ export class PGN {
     }
 
     parseMoves() {
-      // Moves should always be defined
-      const whiteMoves = this.pgn.match(/(?<=(\d)*\. )\S*/g) as string[];
-      const blackMoves = this.pgn.match(/(?<=} )\S*(?= {)/g) as string[];
+      let whiteMoves: string[] = [];
+      let blackMoves: string[] = [];
+
+      /** i.e... 1. (e4) (e5) */
+      const regEx = /\d*\. ([\S]*) ?([\S]*)/g;
+
+      let result;
+      while((result = regEx.exec(this.pgn)) !== null) {
+        const [/* ignore match value when destructuring */, whiteMove, blackMove] = result;
+        if(this.isMove(whiteMove)) whiteMoves.push(this.cleanMove(whiteMove));
+        if(this.isMove(blackMove)) blackMoves.push(this.cleanMove(blackMove));
+      }
+      console.log(whiteMoves);
+      console.log(blackMoves);
       const allMoves = [];
       for(let i = 0; i < whiteMoves.length; i++) {
         const tempMoves = [whiteMoves[i]];
@@ -81,4 +92,31 @@ export class PGN {
       console.log(this.moveTimings?.length);
       console.log(this.moves.length);
     }
+
+    isMove(value: string) {
+      return !(value === '1-0' || value === '0-1' || value === '1/2-1/2');
+    }
+
+    cleanMove(move: string) {
+      return move.replace(/#|\+/g, '');
+    }
 }
+
+// const pgn = new PGN(`[Event "Rated Classical game"]
+// [Site "https://lichess.org/a9tcp02g"]
+// [White "Desmond_Wilson"]
+// [Black "savinka59"]
+// [Result "1-0"]
+// [UTCDate "2012.12.31"]
+// [UTCTime "23:04:12"]
+// [WhiteElo "1654"]
+// [BlackElo "1919"]
+// [WhiteRatingDiff "+19"]
+// [BlackRatingDiff "-22"]
+// [ECO "D04"]
+// [Opening "Queen's Pawn Game: Colle System, Anti-Colle"]
+// [TimeControl "480+2"]
+// [Termination "Normal"]
+
+// 1. d4 d5 2. Nf3 Nf6 3. e3 Bf5 4. Nh4 Bg6 5. Nxg6 hxg6 6. Nd2 e6 7. Bd3 Bd6 8. e4 dxe4 9. Nxe4 Rxh2 10. Ke2 Rxh1 11. Qxh1 Nc6 12. Bg5 Ke7 13. Qh7 Nxd4+ 14. Kd2 Qe8 15. Qxg7 Qh8 16. Bxf6+ Kd7 17. Qxh8 Rxh8 18. Bxh8 1-0`);
+// pgn.print();
